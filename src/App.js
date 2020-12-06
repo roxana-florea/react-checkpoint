9e727ce5390e858ba8e13aafdf96b02839ab6a45
xuleasca <florea.roxana5@yahoo.com>
2020-12-06 17:55:13 +0000

    added filter by categories

--- src/App.js
+++ src/App.js
@@ -1,23 +1,101 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
 
 function App() {
+
+const [todo, setTodo] = useState('');
+const [category, setCategory] = useState('Other')
+const [todoList, setTodoList] = useState([]);
+const [filtered, setFiltered] = useState([]);
+const [isClicked, setIsClicked] = useState(false)
+
+function handleSubmit(ev){
+  setTodo(ev.target.value)
+}
+
+function handleSelect(ev){
+  setCategory(ev.target.value)
+}
+
+function addItem(){
+  setTodoList(prev=>{
+    return [...prev,{name: todo, category: category}]
+  })
+  setTodo('')
+}
+//delete
+function deleteTodo(key){
+  setTodoList(prev=>{
+    return prev.filter((item,i)=>{
+      return i !== key
+    })
+  })
+
+  setFiltered(prev=>{
+    return prev.filter((item,i)=>{
+      return i !== key
+    })
+  })
+  
+}
+//filter
+function filterTodo(categ){
+  setIsClicked(true)
+   setFiltered(todoList.filter(item=>{
+    return item.category === categ
+   }))
+
+
+}
+//show All
+function showAll(){
+  setFiltered(todoList)
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input value={todo} placeholder='add text here...' type='text' onChange={handleSubmit}/>
+     <select onChange={handleSelect}>
+       <option value='Other'>Other</option>
+       <option value='Coding'>Coding</option>
+       <option value='Sports'>Sports</option>
+       <option value='Food'>Food</option>
+     </select>
+     <button onClick={addItem}>Submit</button>
+     {
+       todoList.length > 0 && !isClicked
+      ? 
+        todoList.map((item,i)=>{
+          return(
+            <div>
+            <p key={i}>{item.name} || {item.category}</p><button onClick={()=>deleteTodo(i)}>X</button>
+            </div>
+          ) 
+          
+       })
+      
+      : isClicked ?
+       
+       filtered.map((item,i)=>{
+        return(
+            <div>
+            <p key={i}>{item.name} || {item.category}</p><button onClick={()=>deleteTodo(i)}>X</button>
+            </div>
+          )  
+       })
+      : <p>You have nothing to do</p>
+     }
+
+     <div className='categories'>
+       <h2>Categories:</h2>
+       <p onClick={()=>filterTodo('Other')}>Other</p>
+       <p onClick={()=>filterTodo('Coding')}>Coding</p>
+       <p onClick={()=>filterTodo('Sports')}>Sports</p>
+       <p onClick={()=>filterTodo('Food')}>Food</p>
+       <p onClick={showAll}>All</p>
+            
+     </div>
     </div>
   );
 }
